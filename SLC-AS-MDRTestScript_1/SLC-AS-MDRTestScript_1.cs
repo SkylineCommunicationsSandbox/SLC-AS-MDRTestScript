b9599c7bcfe9484e00d79132ec8cd3c157fa43d9
Mieke Dryepondt <mieke.dryepondt@skyline.be>
2023-09-19 15:19:16 +0200

    update

--- SLC-AS-MDRTestScript_1/SLC-AS-MDRTestScript_1.cs
+++ SLC-AS-MDRTestScript_1/SLC-AS-MDRTestScript_1.cs
@@ -68,7 +68,7 @@ namespace SLC_AS_MDRTestScript_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello again");
+			engine.GenerateInformation("Hello again...");
 		}
 	}
 }
